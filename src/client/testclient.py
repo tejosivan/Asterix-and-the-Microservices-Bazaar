9f91e23f489ced064b51bd677cca0862e035f236
tejosivan <tejosivan@gmail.com>
2025-05-06 21:10:25 -0400

    test client

---
+++ src/client/testclient.py
@@ -0,0 +1,155 @@
+# libraries and setup
+import sys
+import requests
+import random
+import time
+from datetime import datetime
+import os
+
+STOCK_LIST = ["GameStart", "BoarCo", "RottenFishCo", "MenhirCo","CaesarTech","Reneium","Goscinnyium","PiloteCo","DogmatixCo","LutetiaTech"]
+trades_record = []
+lookup_latencies = []
+trade_latencies = []
+order_latencies = []
+
+# main function
+def run_client(p, stock_name):
+    print(f"\n\nEstablishing a session for {stock_name}...")
+    num_of_trades = 0
+    FRONTEND_HOST = os.environ.get("FRONTEND_HOST", "localhost")
+    FRONTEND_PORT = os.environ.get("FRONTEND_PORT", "5555")
+    BASE_URL = f"http://{FRONTEND_HOST}:{FRONTEND_PORT}"
+
+    # Resources referred:
+    # https://docs.python-requests.org/en/latest/user/advanced/#session-objects
+    # https://requests.readthedocs.io/en/latest/user/advanced/
+    with requests.Session() as session:
+        for i in range(5):
+            stock_name = random.choice(STOCK_LIST)
+            while True:
+                #time.sleep(1) #needed?
+                startTime = time.time()
+                lookup_response = session.get(f"{BASE_URL}/stocks/{stock_name}")
+                endTime = time.time()
+                lookup_latencies.append(endTime - startTime)
+                if lookup_response.status_code == 200:
+                    response = lookup_response.json()
+                    price = response["data"]["price"]
+                    quantity = response["data"]["quantity"]
+                    print(
+                        f"\n\n{stock_name} lookup results: \n Price = {price} \n Quantity = {quantity}"
+                    )
+                    if quantity > 0:
+                        request_data = {"name": stock_name, "quantity": 1, "type": "sell"}
+                        startTime = time.time()
+                        trade_response = session.post(
+                            f"{BASE_URL}/orders", json=request_data
+                        )
+                        endTime = time.time()
+                        trade_latencies.append(endTime - startTime)
+                        if trade_response.status_code == 200:
+                            resp = trade_response.json()
+                            transaction_number = resp["data"]["transaction_number"]
+                            print(
+                                f"\n\nOne stock purchased of {stock_name}, transaction_number = {transaction_number}"
+                            )
+                            num_of_trades += 1
+                            record = {
+                                "order_number": transaction_number,
+                                "stock_name": stock_name,
+                                "type": "sell",
+                                "quantity": 1,
+                            }
+                            trades_record.append(record)
+                        else:
+                            print("\n\nTrade Failed, exiting")
+                            break
+                    else:
+                        print("No stock quantity left! Exiting session")
+                        break
+                else:
+                    if lookup_response.status_code == 404:
+                        print("Error: Stock not found!")
+                if random.random() > p:
+                    print(f"\n\nEnd session. {num_of_trades} trades were made.")
+                    break
+
+            # validation block
+            """ to test error
+            faulty_record = {
+                                "order_number": 12345,
+                                "stock_name": "GameStart",
+                                "type": "sell",
+                                "quantity": 1
+                            }
+            trades_record.append(faulty_record)"""
+        for trade in trades_record:
+            transaction_number = trade["order_number"]
+            startTime = time.time()
+            validation_resp = session.get(f"{BASE_URL}/orders/{transaction_number}")
+            endTime = time.time()
+            order_latencies.append(endTime - startTime)
+            if validation_resp.status_code == 200:
+                server_copy_trade = validation_resp.json()["data"]
+                if (
+                    server_copy_trade["name"] == trade["stock_name"]
+                    and server_copy_trade["type"] == trade["type"]
+                    and server_copy_trade["quantity"] == trade["quantity"]
+                ):
+                    print(f"Transaction {transaction_number} correctly verified.")
+                else:
+                    print(
+                        f"Transaction {transaction_number} doesn't match the server's order."
+                    )
+            else:
+                print("error validating")
+
+
+
+
+
+
+
+
+
+
+# driver code
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        try:
+            p = float(sys.argv[1])
+            if not 0 <= p <= 1:
+                print("P needs to be in [0,1], defaulting to 0.8")
+                p = 0.8
+        except ValueError:
+            print("P should be a float! Defaulting to 0.8")
+            p = 0.8
+    else:
+        p = 0.8
+
+    # pick a random stock
+    stock_name = random.choice(STOCK_LIST)
+    run_client(p, stock_name)
+    lookup_latency = sum(lookup_latencies) / len(lookup_latencies)
+    trade_latency = sum(trade_latencies) / len(trade_latencies)
+    order_latency = sum(order_latencies) / len(order_latencies)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    latencydumpfile = f"latency_{timestamp}.txt"
+
+    with open(latencydumpfile, "w") as f:
+        f.write(f"lookup latency: {lookup_latency}\n")
+        f.write(f"trade latency: {trade_latency}\n")
+        f.write(f"order latency: {order_latency}\n")
+        f.write("\n\n\n")
+        f.write("lookup latencies:\n")
+        for i in lookup_latencies:
+            f.write(str(i) + "\n")
+        f.write("trade latencies:\n")
+        for i in trade_latencies:
+            f.write(str(i) + "\n")
+        f.write("order latencies:\n")
+        for i in order_latencies:
+            f.write(str(i) + "\n")
+
+
